1f51215cd1adeafc44efbcef5d9d9b187726f42e
prathameshpatil5181 <prathameshpatil5181@gmail.com>
2024-10-13 19:44:38 +0530

    origin changes on server

--- apps/server/src/index.ts
+++ apps/server/src/index.ts
@@ -3,7 +3,7 @@ import http from "http";
 const PORT = process.env.PORT ? process.env.PORT : 8000;
 
 //express imports
-import express, { Request,Response,NextFunction } from "express";
+import express, { Request, Response, NextFunction } from "express";
 import cors from "cors";
 const app = express();
 import bodyParser = require("body-parser");
@@ -19,22 +19,24 @@ import searchRouter from "./routes/search";
 const server = http.createServer(app);
 app.use(bodyParser.urlencoded({ extended: false, limit: "20mb" }));
 
-
 // parse application/json
-app.use(bodyParser.json({
-  limit:'20mb'
-}));
+app.use(
+  bodyParser.json({
+    limit: "20mb",
+  })
+);
 app.use(
   cookieParser({
     extended: true,
   })
 );
 
-
 const allowedOrigins = [
   "https://chat-application-3ew6hxl93-prathameshpatil5181s-projects.vercel.app", // Your frontend on Vercel
   "https://chat.prathamesh-de.me", // Your backend
-  "http://localhost:3000"
+  "http://localhost:3000",
+  "https://chat-application-web.vercel.app",
+
   // Add other trusted origins if needed
 ];
 
@@ -48,27 +50,27 @@ app.use(
       if (allowedOrigins.includes(origin)) {
         return callback(null, true);
       } else {
-        return callback(new Error('Not allowed by CORS'));
+        return callback(new Error("Not allowed by CORS"));
       }
     },
     credentials: true,
   })
 );
 
-
-
-const gotrequest = (req:Request, res:Response, next:NextFunction) => {
+const gotrequest = (req: Request, res: Response, next: NextFunction) => {
   console.log(`request from ${req.socket.remoteAddress}`);
   next();
-}
-app.get("/", (req:Request, res:Response) => {
+};
+app.get("/", (req: Request, res: Response) => {
   res.send("healthy");
-})
-app.use("/auth",gotrequest ,authRouter);
+});
+app.use("/auth", gotrequest, authRouter);
 app.use("/user", userRouter);
 app.use("/searchUser", searchRouter);
 app.get("/hello", (req, res) => {
-  res.status(200).send(`<h1>hello from server to ${req.socket.remoteAddress}</h1>`)
+  res
+    .status(200)
+    .send(`<h1>hello from server to ${req.socket.remoteAddress}</h1>`);
 });
 // websocket connection
 const newConnection = new websocketconnection(server);
